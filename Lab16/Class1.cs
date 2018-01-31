bb629c39c6696fa81acc124155763466b5bafb3e
famouschamus <chamusg@gmail.com>
2018-01-31 16:29:43 -0500

    initial commit

---
+++ Lab16/Class1.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Lab16
+{
+    class Prime
+    {
+        [Test]
+        public static void Prime0()
+        {
+            int num1 = 4;
+
+            for (int a = 2; a <= num1 / 2; a++)
+            {
+                if (num1 % a == 0)
+                {
+
+                    Assert.AreEqual(num1, num1, "wrong");
+                }
+
+            }
+
+        }
+    }
+}
+
+
+
---
+++ Lab16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab16
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Give me prime number");
+            int num1 = Int32.Parse(Console.ReadLine());
+            if (num1 < 2)
+            {
+                Console.WriteLine("Not a Prime");
+                Main();
+            }
+            else
+            {
+                for (int a = 2; a <= num1 / 2; a++)
+                {
+                    if (num1 % a == 0)
+                    {
+                        Console.WriteLine("Not a Prime");
+                        Main();
+                    }
+                }
+            }
+        }
+
+    }
+}
